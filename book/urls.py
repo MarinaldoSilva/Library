f19696f35205584caddd889fddb7a89d47858d0f
Mario <marinaldo12@hotmail.com>
2025-11-20 16:10:46 -0300

    criação das rotas e modificações de viewset para apiview

--- book/urls.py
+++ book/urls.py
@@ -1,10 +1,52 @@
 from django.urls import URLPattern, path
 
-from .views import BookListCreateAPIView, BookRetrieveUpdateDestroyAPIView
+from .views import (
+    BookListAPIView,
+    BookCreateAPIView,
+    BookDetailAPIView,
+    BookUpdateAPIView,
+    BookDeleteAPIView
+)
 
-urlpatterns: URLPattern = [
-    path("books/", BookListCreateAPIView.as_view(), name="books_list_create"),
-    path(
-        "books/<pk>/", BookRetrieveUpdateDestroyAPIView.as_view(), name="books_detail"
-    ),
+
+urlpatterns = [
+    path('books/', BookListAPIView.as_view(), name='book-list'),
+    path('books/create/', BookCreateAPIView.as_view(), name='book-create'),
+    path('books/<uuid:pk>/', BookDetailAPIView.as_view(), name='book-detail'),
+    path('books/<uuid:pk>/update/', BookUpdateAPIView.as_view(), name='book-update'),
+    path('books/<uuid:pk>/delete/', BookDeleteAPIView.as_view(), name='book-delete'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# urlpatterns: URLPattern = [
+#     path("books/", BookListCreateAPIView.as_view(), name="books_list_create"),
+#     path(
+#         "books/<pk>/", BookRetrieveUpdateDestroyAPIView.as_view(), name="books_detail"
+#     ),
+# ]
