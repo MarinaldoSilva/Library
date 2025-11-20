5f782b4d5a0d190e80fe3b1a3a0c6667f6cfddcf
Mario <marinaldo12@hotmail.com>
2025-11-20 16:11:09 -0300

    criação das rotas e modificações de viewset para apiview

--- book/views.py
+++ book/views.py
@@ -1,24 +1,119 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Book
 from .serializer import BookSerializer
 
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookListAPIView(APIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = BookSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_superuser or user.is_staff:
-            return Book.objects.all()
-        return Book.objects.filter(author__name=user)
-
-
-class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class BookCreateAPIView(APIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = BookSerializer
 
-    def get_queryset(self):
-        return Book.objects.filter(author=self.request.user)
+    def post(self, request):
+        serializer =  BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class BookDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        try:
+            book = Book.objects.get(id=pk)
+        except Book.DoesNotExist:
+            return Response({"error": "Livro não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class BookUpdateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, pk):
+        try:
+            book = Book.objects.get(id=pk)
+            serializer = BookSerializer(instance=book, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response({"error":"Livro não encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+class BookDeleteAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        try:
+            book = Book.objects.get(id=pk)
+            book.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Book.DoesNotExist:
+            return Response({"error":"Livro não localizado"}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class BookListCreateAPIView(generics.ListCreateAPIView):
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = BookSerializer
+
+#     def get_queryset(self):
+#         user = self.request.user
+#         if user.is_superuser or user.is_staff:
+#             return Book.objects.all()
+#         return Book.objects.filter(author__name=user)
+
+
+# class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = BookSerializer
+
+#     def get_queryset(self):
+#         return Book.objects.filter(author=self.request.user)
