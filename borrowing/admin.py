c9df2cea42f3c21d1efbb49d0cdb0a9c28672b3f
Mario <marinaldo12@hotmail.com>
2025-12-09 11:27:10 -0300

    criação do borrowing no admin

--- borrowing/admin.py
+++ borrowing/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Borrowing
 
-# Register your models here.
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = ("user", "book", "borrow_date", "return_date", "status")
+    list_filter = ("status", "borrow_date", "return_date")
+    search_fields = ("book__title",)
+    ordering = ("-borrow_date",)
