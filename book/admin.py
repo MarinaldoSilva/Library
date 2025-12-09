16a2bc3bcd409dd724c5864d70e8311917affbb6
Mario <marinaldo12@hotmail.com>
2025-12-09 11:26:04 -0300

    criação do book no admin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "category", "status", "last_edition")
+    list_filter = ("status", "category", "created_at")
+    search_fields = ("title", "ISBN", "author__name")
+    ordering = ("title",)
+    
