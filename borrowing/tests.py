7086b7ea08d9968df48e66dba0feb37ec74bd1a5
Marinaldo Silva <97764197+MarinaldoSilva@users.noreply.github.com>
2025-12-09 11:16:16 -0300

    Fix test for inactive borrowing user status
    atualização dos testes de empréstimos que estavam falhando

--- borrowing/tests.py
+++ borrowing/tests.py
@@ -44,7 +44,7 @@ class BorrowingTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_not_borroeing_user_inactiv(self):
-
+ 
         self.user.status = False
         self.user.save()
 
