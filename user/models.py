21f81d0e4c5a6e1a7bb8c7ddbe115dc26cfa25aa
Marinaldo Silva <97764197+MarinaldoSilva@users.noreply.github.com>
2025-12-09 11:12:16 -0300

    Implement user profile management API views
    Added API views for user profile management, including viewing, updating, and deleting user profiles.

--- user/models.py
+++ user/models.py
@@ -1,22 +1,42 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializer import UserSerializer
+
+User = get_user_model()
 
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=True, null=False)
-    birth_date = models.DateField()
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    username = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=128)
-    full_name = models.CharField(max_length=200)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now_add=True)
-    is_staff = models.BooleanField(default=True)
-    status = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
+class MyProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    REQUIRED_FIELDS = ["birth_date", "full_name"]
+    @extend_schema(summary="Visualizar meu perfil", description="Retorna os dados do perfil do usuário logado.", responses={200: UserSerializer})
+    def get(self, request) -> Response:
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def __str__(self):
-        return f"Usuário {self.username} criado com sucesso."
+
+class UpdateProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(summary="Atualizar meu perfil", description="Atualiza parcialmente os dados do usuário.", request=UserSerializer, responses={200: UserSerializer})
+    def patch(self, request):
+        user = request.user
+        serializer = UserSerializer(instance=user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserDeleteAPIView(APIView):
+    permission_classes = [IsAdminUser]
+
+    @extend_schema(summary="Deletar usuário", description="Exclui um usuário específico pelo ID. Apenas para administradores.", responses={204: None})
+    def delete(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
