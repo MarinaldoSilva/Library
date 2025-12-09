bca0940516dca638af65cfe133f4c84679277452
Mario <marinaldo12@hotmail.com>
2025-12-09 11:28:59 -0300

    criação do user no admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "full_name","status")
+    list_filter = ("status",)
+    search_fields = ("username", "email", "full_name")
+    ordering = ("username",)
+    
+admin.site.register(User, CustomUserAdmin)
